57cfcf2230e9b756ec3509283abf53ce542433e3
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-14 15:30:17 +0700

    fix: update to previous code

--- src/pipeline.runner.ts
+++ src/pipeline.runner.ts
@@ -52,51 +52,6 @@ export class PipelineRunner {
     }
   }
 
-  public async MonitorDeployment(
-    webApi: azdev.WebApi,
-    release: ReleaseInterfaces.Release
-  ) {
-    const releaseId = release.id;
-    const projectName = UrlParser.GetProjectName(
-      this.taskParameters.azureDevopsProjectUrl
-    );
-    const releaseApi = await webApi.getReleaseApi();
-
-    const res = await releaseApi.getRelease(projectName, releaseId);
-
-    const releaseStatus = {
-      0: "undefined",
-      1: "not started",
-      2: "in progress",
-      4: "succeeded",
-      8: "cancelled",
-      16: "rejected",
-      32: "queued",
-      64: "scheduled",
-      128: "partially succeeded",
-    };
-
-    for (const environment of res.environments) {
-      console.log(`ENVIRONMENT (${environment.name})`, {
-        status: releaseStatus[environment.status],
-      });
-    }
-
-    const retryStatuses = [
-      ReleaseInterfaces.EnvironmentStatus.Succeeded,
-      ReleaseInterfaces.EnvironmentStatus.Canceled,
-      ReleaseInterfaces.EnvironmentStatus.Rejected,
-    ];
-    if (res.environments.find((item) => !retryStatuses.includes(item.status))) {
-      await new Promise((res) => {
-        setTimeout(() => {
-          this.MonitorDeployment(webApi, release);
-          res(true);
-        }, 4500);
-      });
-    }
-  }
-
   public async RunYamlPipeline(webApi: azdev.WebApi): Promise<any> {
     let projectName = UrlParser.GetProjectName(
       this.taskParameters.azureDevopsProjectUrl
@@ -277,9 +232,6 @@ export class PipelineRunner {
       log.LogPipelineTriggerOutput(release);
       if (release != null && release._links != null) {
         log.LogOutputUrl(release._links.web.href);
-      }
-      if (this.taskParameters.azurePipelineMonitorDeployment) {
-        await this.MonitorDeployment(webApi, release);
       }
     }
   }
